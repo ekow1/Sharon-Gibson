05b408b8f48625a7420afbee0a5d8bdb0fe68404
ekow <ekowfirmino@yahoo.com>
2022-11-28 00:48:09 +0000

    updated-calendly-user

--- src/components/Calendly.js
+++ src/components/Calendly.js
@@ -5,7 +5,7 @@ const Calendly = () => {
   return (
     <div className="App">
     <PopupButton
-      url="https://calendly.com/ekowfirmino"
+      url="https://calendly.com/iamsharongibson"
       /*
        * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
        * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
